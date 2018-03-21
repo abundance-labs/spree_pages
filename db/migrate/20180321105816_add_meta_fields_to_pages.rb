f34fdbbd3f7d5431ede80ce55312a8836907bd82
Artem Kozaev <artemkozaev@gmail.com>
2018-03-21 22:32:52 +0300

    Use activerecord 5.0 in migrations

--- db/migrate/20180321105816_add_meta_fields_to_pages.rb
+++ db/migrate/20180321105816_add_meta_fields_to_pages.rb
@@ -1,4 +1,4 @@
-class AddMetaFieldsToPages < ActiveRecord::Migration[5.1]
+class AddMetaFieldsToPages < ActiveRecord::Migration[5.0]
   def change
     add_column :spree_pages, :meta_title, :string
     add_column :spree_pages, :meta_description, :string
--- db/migrate/20180321122609_add_live_to_spree_pages.rb
+++ db/migrate/20180321122609_add_live_to_spree_pages.rb
@@ -1,4 +1,4 @@
-class AddLiveToSpreePages < ActiveRecord::Migration[5.1]
+class AddLiveToSpreePages < ActiveRecord::Migration[5.0]
   def change
     add_column :spree_pages, :live, :boolean, default: true
   end
