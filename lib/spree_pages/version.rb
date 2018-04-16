53b4d4c151f8ca650589b58a84959a612ba6d971
Artem Kozaev <artemkozaev@gmail.com>
2018-04-16 13:16:38 +0300

    Change initial version

--- lib/spree_pages/version.rb
+++ lib/spree_pages/version.rb
@@ -4,15 +4,14 @@ module SpreePages
   # Returns the version of the currently loaded SpreePages as a
   # <tt>Gem::Version</tt>.
   def version
-    Gem::Version.new VERSION::STRING
+    Gem::Version.new(VERSION::STRING)
   end
 
   module VERSION
     MAJOR = 0
     MINOR = 0
     TINY  = 1
-    PRE   = 'alpha'.freeze
 
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
 end
